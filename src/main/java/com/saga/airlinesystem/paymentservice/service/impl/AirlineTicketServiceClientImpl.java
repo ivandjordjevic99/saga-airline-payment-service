4a5fe14aabf19decc9c15ea5d63849fc9a88d6b5
Ivan Djordjevic <ivandjordjevic@Ivans-MacBook-Pro.local>
2026-03-18 17:20:40 +0100

    Fix logging

--- src/main/java/com/saga/airlinesystem/paymentservice/service/impl/AirlineTicketServiceClientImpl.java
+++ src/main/java/com/saga/airlinesystem/paymentservice/service/impl/AirlineTicketServiceClientImpl.java
@@ -3,11 +3,13 @@ package com.saga.airlinesystem.paymentservice.service.impl;
 import com.saga.airlinesystem.paymentservice.dto.TicketOrderUpdateRequest;
 import com.saga.airlinesystem.paymentservice.dto.TicketOrderUpdateResponse;
 import com.saga.airlinesystem.paymentservice.service.AirlineTicketServiceClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class AirlineTicketServiceClientImpl implements AirlineTicketServiceClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
@@ -28,7 +30,7 @@ public class AirlineTicketServiceClientImpl implements AirlineTicketServiceClien
             );
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
             return null;
         }
     }
